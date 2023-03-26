ad9b0c6241809a5a90d3a5191c322f3979fb786e
adarshj12 <adarshjayadevan12@gmail.com>
2023-03-26 18:25:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { authentication } from './firebase';
+import { useState } from 'react';
 
 function App() {
+  const [mobile, setMobile] = useState()
+  const [otp, setOtp] = useState();
+
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': (response) => {
+        console.log(response);
+      },
+      defaultCountry: "IN"
+    }, authentication);
+  }
+
+  const requestOTP = (e) => {
+    e.preventDefault();
+    const phoneNumber = '+91' + mobile
+    generateRecaptcha();
+    let appVerifier = window.recaptchaVerifier
+    signInWithPhoneNumber(authentication, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        console.log(`confirmation of otp => ${JSON.stringify(confirmationResult)}`);
+        // ...
+      }).catch((error) => {
+        console.log(`error=> ${error.message}`);
+      });
+  }
+  const verifyOTP = (e) => {
+    e.preventDefault();
+    let confirmationResult = window.confirmationResult;
+    confirmationResult.confirm(otp).then((result) => {
+      console.log(result);
+      console.log(JSON.stringify(result));
+      const user = result.user;
+      console.log(`signed in successfully as ${JSON.stringify(user)}`);
+      // ...
+    }).catch((error) => {
+      console.log(`error=> ${error.message}`);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Enter Mobile Number</h1>
+      <form onSubmit={requestOTP}  >
+        <div id='sign-in-button'></div>
+        <input type='number' placeholder='enter number' id='recaptcha-container' onChange={(e) => setMobile(e.target.value)}></input>
+        <input type="submit" value="ok" />
+      </form>
+
+      <h1>Enter OTP</h1>
+      <form onSubmit={verifyOTP}>
+        <input type='number' placeholder='enter number' id='recaptcha-container' onChange={(e) => setOtp(e.target.value)} ></input>
+        <input type="submit" value="ok" />
+      </form>
     </div>
   );
 }
 
-export default App;
+export default App; 
